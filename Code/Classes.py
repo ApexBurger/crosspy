ccfbd537ea011142cc8cd5f8f6d709b182c0918e
Mona Lisa <alex.bergsmo@gmail.com>
2020-03-18 18:39:04 +0000

    Added filters to the classes

--- Code/Classes.py
+++ Code/Classes.py
@@ -8,6 +8,7 @@ import numpy as np
 
 import os
 
+import filters
 
 
 class Imset:
@@ -68,15 +69,67 @@ class Imset:
 
         return imarray_conv
 
+    # Filter settings are currently only definable here:
+    filter_settings = [4,2,16,32]
+    # TO DO - GUI to help user chose the filter
+
+    def gen_filters(self, size_pass, overlap_pass):
+            # Inputs:
+        #   roisize = subset size - (128, 256 etc.)
+        #   fpasset = [high pass cut off, high pass width, low pass cut off, low pass width]
+        # outputs:
+        #   fftfilter = non idea (gaussian) band pass filter
+        #   hfilter = hanning filter
+
+        pi = np.pi
+        cos = np.cos
+        dot = np.dot
+        sqrt = np.sqrt
+        exp = np.exp
+
+        lcutoff = fpassset[2]
+        lwidth = fpassset[3]/2
+        hcutoff = fpassset[0]
+        hwidth = fpassset[1]/2
+
+        if lcutoff < hcutoff:
+            print('low pass filter smaller than high pass filter')
+            return
+
+        # generate square grid
+        u = range(0, roisize)
+        # meshgrid function
+        meshv, meshu = np.meshgrid(u,u)
+        meshvf = meshv-roisize/2-0.5
+        meshuf = meshu-roisize/2-0.5
 
 
-    def imfilter(self, size_pass_1, overlap_pass_1, size_pass_2, overlap_pass_2):
+        # create Hann window
+        hfilter = (cos(((pi*(meshuf)/roisize)))*(cos((pi*meshvf/roisize))))
 
-        # this method sets up the filter windows and boundaries for ROIs
+        # create fft filter
 
+        distf = sqrt((meshvf*meshvf)+(meshuf*meshuf))
 
+        # lowpass
+        lfftfilter = exp(-((distf-lcutoff)/sqrt(2)*lwidth/2)**2)
+        # need to iterate through the array to satisfy expression:
+        lfftfilter[distf > (lcutoff+2*lwidth)] = 0
+        lfftfilter[distf < (lcutoff+2*lwidth)] = 1
+        # distf < lcutoff = 1
 
-        return fft_filter, h_filter
+        # highpass
+
+        hfftfilter = exp(-((hcutoff-distf)/sqrt(2)*hwidth/2))**2
+        hfftfilter[distf < (hcutoff-2*hwidth)] = 0
+        hfftfilter[distf > hcutoff] = 1
+
+        # combine
+
+        fftfilter = hfftfilter*lfftfilter
+        fftfilter = np.fft.fftshift(fftfilter) #THIS IS A MATLAB FUNCTION - find python equivalent
+        
+        return fftfilter, hfilter
 
 
 
