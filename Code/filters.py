7399fa740e11c1b4a5639036b00c02a408985180
Mona Lisa <alex.bergsmo@gmail.com>
2020-03-18 17:57:51 +0000

    Finished filters.py

--- Code/filters.py
+++ Code/filters.py
@@ -1,5 +1,8 @@
 # Filters
 # This file contains imaging filters for the main DIC script
+# Current filters:
+#   - Han window
+#   - FFT filter
 # Written by Alexander Bergsmo 2020
 # Imperial College London 
 # ab12215@ic.ac.uk
@@ -12,6 +15,7 @@ def xfilters(roisize, fpassset):
     # outputs:
     #   fftfilter = non idea (gaussian) band pass filter
     #   hfilter = hanning filter
+
     pi = np.pi
     cos = np.cos
     dot = np.dot
@@ -28,37 +32,36 @@ def xfilters(roisize, fpassset):
         return
 
     # generate square grid
-    u = range(1, roisize)
+    u = range(0, roisize)
     # meshgrid function
     meshv, meshu = np.meshgrid(u,u)
     meshvf = meshv-roisize/2-0.5
     meshuf = meshu-roisize/2-0.5
 
 
-    # create Hanning Filter
+    # create Hann window
     hfilter = (cos(((pi*(meshuf)/roisize)))*(cos((pi*meshvf/roisize))))
 
     # create fft filter
 
-    distf = sqrt(dot(meshvf*meshvf)+(meshuf*meshuf))
+    distf = sqrt((meshvf*meshvf)+(meshuf*meshuf))
 
     # lowpass
-    lfftfilter = exp(-((distf-lcutoff)/sqrt(2)*lwidth/2))**2)
+    lfftfilter = exp(-((distf-lcutoff)/sqrt(2)*lwidth/2)**2)
     # need to iterate through the array to satisfy expression:
-    # distf > (lcufoff+2*lwidth) = 0 and
+    lfftfilter[distf > (lcutoff+2*lwidth)] = 0
+    lfftfilter[distf < (lcutoff+2*lwidth)] = 1
     # distf < lcutoff = 1
 
     # highpass
 
-    hfftfilter = exp(-((hcutoff-distf)/sqrt(2)*hwidth/2))**2)
-    # need to iterate through array to satisfy
-    # distf < (hcutoff-2*hwidth) = 2 and
-    # distf > hcutoff = 1
+    hfftfilter = exp(-((hcutoff-distf)/sqrt(2)*hwidth/2))**2
+    hfftfilter[distf < (hcutoff-2*hwidth)] = 0
+    hfftfilter[distf > hcutoff] = 1
 
     # combine
 
     fftfilter = hfftfilter*lfftfilter
-    fftfilter = fftshift(fftfilter) #THIS IS A MATLAB FUNCTION - find python equivalent
+    fftfilter = np.fft.fftshift(fftfilter) #THIS IS A MATLAB FUNCTION - find python equivalent
     
-    return fftfilter, hfilter
     return fftfilter, hfilter
