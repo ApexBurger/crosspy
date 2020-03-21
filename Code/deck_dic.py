9fd62c45cd5c0eaf58fc7c21915159485df2c571
Tom <45037639+tmcaul@users.noreply.github.com>
2020-03-21 11:47:34 +0000

    update dic class for plotting etc

--- Code/deck_dic.py
+++ Code/deck_dic.py
@@ -1,45 +1,46 @@
 #%%
 %load_ext autoreload
 %autoreload 2
-
-roi = dict(size_pass = 200, overlap_pass = 0.7, xcf_mesh=500)
-filter_settings=[4,2,15,8] #fft filter settings: high pass, high pass width, low pass, low pass width
-
+#%%
 import os as o
 o.chdir('/Users/tom/Documents/GitHub/crosspy/Code')
 from Classes import *
 from imprep_functions import *
 from XCF_functions import *
 from pathlib import Path
-import matplotlib.pyplot as plt 
 
-folder_path = Path(r"/Users/tom/Documents/GitHub/crosspy/data")
+folder_path = Path(r"/Users/tom/Documents/GitHub/crosspy/data/Siyang")
 Images = Imset(folder_path,'tif')
 
 # %% Instantiate and run the DIC
 
+#fft filter settings: high pass, high pass width, low pass, low pass width
+filter_settings=[4,2,15,8]
+roi_1stpass = dict(size_pass = 200, overlap_pass = 0.7, xcf_mesh=500)
+
 #build the dic class (but don't run it yet):
-dic_1stpass=dic(Images,roi,filter_settings)
+dic_1stpass=dic(Images,roi_1stpass,filter_settings)
 
 #run the dic on specified images within the stack, and get displacements:
-dx_map, dy_map, ph_map = dic_1stpass.run(imnos=[0,1]) 
+dic_1stpass.run_sequential() #figures out imnos as consecutive images
 
 #can also do:
-#dx_maps, dy_maps, ph_maps = dic_1stpass.run_sequential() #figures out imnos as consecutive images
 #dx_maps, dy_maps, ph_maps = dic_1stpass.run_cumulative() #figures out imnos as 0 and sequential
 
-#%% Generate filters - ALEX
+#%% Show the displacements
+dic_1stpass.plot_results()
 
-fftfil, hfil = Imset.gen_filters(roi['size_pass'])
+#%% Image correction
+Images_cor=Images.correct(dx_map,dy_map)
 
-#%% Run cross correlation - TOM
+#%% Second pass
+roi_2ndpass = dict(size_pass = 200, overlap_pass = 0.7, xcf_mesh=500)
+dic_2ndpass=dic(Images_cor,roi_2ndpass,filter_settings)
 
-shift_x, shift_y, peakcc = xcf()
+dx_maps, dy_maps, ph_maps = dic_2ndpass.run_sequential() #figures out imnos as consecutive images
 
 #%% Image correction - ALEX
 
 image_cor = im_correct(Images, shift_x, shift_y, ss_locations)
-
-#%% Re-run cross correlation with smaller subset size - TOM
 
 #%% calculate strain - ALEX
